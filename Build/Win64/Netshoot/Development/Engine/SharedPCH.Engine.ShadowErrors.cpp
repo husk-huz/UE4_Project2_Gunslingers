dd42c44d03870bb55a36139886ecf7bcb7151e99
husk-huz <75027733+husk-huz@users.noreply.github.com>
2022-07-30 16:38:18 +0800

    Add files via upload

---
+++ Build/Win64/Netshoot/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30143
+#include "D:/UE4/Project/tmp/Netshoot/Intermediate/Build/Win64/Netshoot/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # UE4_Project2_Gunslingers
 第二周作业
+存在联机状态下击中无法加分以及无法实现同步移动的bug
